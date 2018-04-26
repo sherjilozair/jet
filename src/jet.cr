28f6f8d0ee68ad345928643db7914a6b8404344e
Bar Hofesh <bar.hofesh@gmail.com>
2018-04-26 11:47:59 +0300

    name spacing

--- src/jet.cr
+++ src/jet.cr
@@ -1,4 +1,18 @@
 require "./jet/**"
 
 module Jet
+  extend self
+
+  def check_success(status : LibCUDA::ErrorT)
+    raise status.to_s unless LibCUDA::ErrorT::Success == status
+  end
+
+  def malloc(size) : Pointer(Void)
+    check_success(LibCUDA.malloc(out ptr, size))
+    ptr
+  end
+
+  def free(ptr)
+    check_success(LibCUDA.free(ptr))
+  end
 end
--- src/jet/jet.cr
+++ src/jet/jet.cr
@@ -1,6 +1,4 @@
 module Jet
-  extend self
-
   class Handler
     getter :handler
 
@@ -39,18 +37,5 @@ module Jet
     def destroy
       LibCuDNN.destroy_tensor_descriptor(@desc)
     end
-  end
-
-  def check_success(status : LibCUDA::ErrorT)
-    raise status.to_s unless LibCUDA::ErrorT::Success == status
-  end
-
-  def malloc(size) : Pointer(Void)
-    check_success(LibCUDA.malloc(out ptr, size))
-    ptr
-  end
-
-  def free(ptr)
-    check_success(LibCUDA.free(ptr))
   end
 end
