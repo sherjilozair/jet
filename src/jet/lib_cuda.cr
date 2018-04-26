0164a701fece3f62b0a7fb50e47ddae65a2ef98b
Bar Hofesh <bar.hofesh@gmail.com>
2018-04-26 12:13:47 +0300

    all cuda bindings done

--- src/jet/lib_cuda.cr
+++ src/jet/lib_cuda.cr
@@ -5,4 +5,6 @@ lib LibCUDA
   end
   fun malloc = cudaMalloc(ptr : Void**, size : LibC::Int) : ErrorT
   fun free = cudaFree(ptr : Void*) : ErrorT
+  fun device_sync = cudaDeviceSynchronize : ErrorT
+  fun check_error = cudaGetLastError : ErrorT
 end
